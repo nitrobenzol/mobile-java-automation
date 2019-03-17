cec9f2ce46919694e201654de63b8c0f95f21506
nitrobenzol <glebsarkisov@gmail.com>
2019-03-17 18:41:27 +0300

    created private field class_number inside MainClass; created getClassNumber method to return class_number; added test to ensure class_number value is bigger than 45

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,12 @@
 public class MainClass
 {
+    private int class_number = 20;
+
+    public int getClassNumber()
+    {
+        return this.class_number;
+    }
+
     public int getLocalNumber()
     {
         int local_number = 14;
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,10 @@ public class MainClassTest extends MainClass
     {
         Assert.assertTrue(getLocalNumber() + " is not equal to 14!", getLocalNumber()==14);
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue(getClassNumber() + " is not bigger than 45!",getClassNumber() > 45);
+    }
 }
